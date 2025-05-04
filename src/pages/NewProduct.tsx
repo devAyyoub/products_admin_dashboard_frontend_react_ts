185f2e32e03137e0cadc23e6fa2775b0456fea92
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-04 20:39:34 +0200

    feat: add form for new product registration and integrate action handler

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,10 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, Form } from "react-router-dom";
+
+export async function action() {
+    console.log('desde action...');
+    
+}
 
 export default function NewProduct() {
   return (
     <>
       <div className="flex justify-between">
-        <h2 className="text-4xl font-black text-slate-500">Registrar producto</h2>
+        <h2 className="text-4xl font-black text-slate-500">
+          Registrar producto
+        </h2>
         <Link
           className="rounded-md bg-indigo-600 hover:bg-indigo-500 p-3 text-white font-bold text-sm shadow-sm"
           to="/"
@@ -12,6 +19,37 @@ export default function NewProduct() {
           Volver a productos
         </Link>
       </div>
+      <Form className="mt-10" method="POST">
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="name">
+            Nombre Producto:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">
+            Precio:
+          </label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
     </>
   );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,21 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Products from "./pages/Products";
-import NewProduct from "./pages/NewProduct";
+import NewProduct, { action as newProductAction } from "./pages/NewProduct";
 
 export const router = createBrowserRouter([
-    {
-        path: '',
-        element: <Layout/>,
-        children: [
-            {
-                index: true,
-                element: <Products/>
-            },
-            {
-                path: 'productos/nuevo',
-                element: <NewProduct/>
-            }
-        ]
-    }
-])
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Products />,
+      },
+      {
+        path: "productos/nuevo",
+        element: <NewProduct />,
+        action: newProductAction
+      },
+    ],
+  },
+]);
