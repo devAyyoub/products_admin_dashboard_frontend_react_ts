a95a2529ab9503a6a580730a75cc06502174add2
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-04 19:44:05 +0200

    refactor: update Layout component structure and styling

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,18 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-    <div>Desde Layout</div>
-    <Outlet/>
+      <header className="bg-slate-800">
+        <div className="mx-auto max-w-6xl py-10">
+          <h1 className="text-4xl font-extrabold text-white">
+            Administrador de productos
+          </h1>
+        </div>
+      </header>
+      <main className="mt-10 mx-auto max-w-6xl p-10 bg-blue-50 shadow">
+        <Outlet />
+      </main>
     </>
-  )
+  );
 }
