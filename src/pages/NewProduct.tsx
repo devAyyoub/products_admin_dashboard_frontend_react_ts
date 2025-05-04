50452a09de71342745ed01bc30554d0818c4ceab
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-04 19:53:35 +0200

    feat: implement NewProduct and update Products components with layout and links

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function NewProduct() {
   return (
-    <div>NewProduct</div>
-  )
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Registrar producto</h2>
+        <Link
+          className="rounded-md bg-indigo-600 hover:bg-indigo-500 p-3 text-white font-bold text-sm shadow-sm"
+          to="/"
+        >
+          Volver a productos
+        </Link>
+      </div>
+    </>
+  );
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom"
+
 export default function Products() {
   return (
-    <div>Products</div>
+    <>
+        <div className="flex justify-between">
+            <h2 className="text-4xl font-black text-slate-500">productos</h2>
+            <Link className="rounded-md bg-indigo-600 hover:bg-indigo-500 p-3 text-white font-bold text-sm shadow-sm" to="productos/nuevo">
+                Agregar producto
+            </Link>
+        </div>
+    </>
   )
 }
