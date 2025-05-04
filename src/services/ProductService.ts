1605209520acc10153b363e2095a7c58ce475833
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-04 21:52:20 +0200

    feat: implement DraftProductSchema for product validation in ProductService

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,8 +1,22 @@
-type ProductData = {
-    [k: string]: FormDataEntryValue
-}
+import { safeParse } from "valibot";
+import { DraftProductSchema } from "../types";
 
-export function addProduct (data : ProductData) {
-    console.log(data);
-    
+type ProductData = {
+  [k: string]: FormDataEntryValue;
+};
+
+export function addProduct(data: ProductData) {
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price,
+    });
+    if (result.success) {
+      console.log(result.output);
+    } else {
+      throw new Error("Datos no válidos");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
