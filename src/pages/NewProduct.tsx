ac18abf387173201570015ae42494a984970dc6a
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-05 20:47:56 +0200

    feat: update addProduct function to handle async operations and remove unnecessary console log

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,4 +1,4 @@
-import { Link, Form, useActionData, ActionFunctionArgs } from "react-router-dom";
+import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductService";
 
@@ -13,7 +13,9 @@ export async function action({ request } : ActionFunctionArgs) {
     return error;
   }
 
-  addProduct(data)
+  await addProduct(data)
+
+  return redirect('/')
 }
 
 export default function NewProduct() {
