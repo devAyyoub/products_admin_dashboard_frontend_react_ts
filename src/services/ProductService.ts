25a25621d538fc809348aee238971dbcf08d9ce8
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-05-05 20:22:27 +0200

    feat: add axios for API requests and update addProduct function to handle async operations

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,18 +1,27 @@
 import { safeParse } from "valibot";
 import { DraftProductSchema } from "../types";
+import axios from "axios";
 
 type ProductData = {
   [k: string]: FormDataEntryValue;
 };
 
-export function addProduct(data: ProductData) {
+export async function addProduct(data: ProductData) {
   try {
     const result = safeParse(DraftProductSchema, {
       name: data.name,
       price: +data.price,
     });
     if (result.success) {
-      console.log(result.output);
+      const url = `${import.meta.env.VITE_API_URL}/api/products`;
+      
+      const { data } = await axios.post(url, {
+        name: result.output.name,
+        price: result.output.price
+      });
+
+      console.log(data);
+      
     } else {
       throw new Error("Datos no válidos");
     }
